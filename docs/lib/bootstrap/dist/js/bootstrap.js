5a7beb4b360298e1822e4cd1ee71265b01903327
pmonitor0 <brferenc@gmail.com>
2025-03-19 01:36:46 +0100

    Publish Blazor client to docs for GitHub Pages

--- docs/lib/bootstrap/dist/js/bootstrap.js
+++ docs/lib/bootstrap/dist/js/bootstrap.js
@@ -1147,7 +1147,7 @@
     [ARROW_RIGHT_KEY$1]: DIRECTION_LEFT
   };
   const Default$b = {
-    interval: 5145,
+    interval: 5283,
     keyboard: true,
     pause: 'hover',
     ride: false,
@@ -4317,7 +4317,7 @@
   const Default = {
     animation: true,
     autohide: true,
-    delay: 5145
+    delay: 5283
   };
 
   /**
